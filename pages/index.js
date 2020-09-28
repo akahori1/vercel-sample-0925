dbe1952ba7f4c373c2dc59132af6b3791ae94c24
akahori_shotaro <AKASAN824@akahori-shotaro-no-MacBook-Air.local>
2020-09-28 21:47:22 +0900

    ０９２８最後のコミット

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>違うパソコンでGitを変更してみたけどさらにいつものパソコンで変更したよ！</title>
+        <title>０９２８全部変えたで！</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
